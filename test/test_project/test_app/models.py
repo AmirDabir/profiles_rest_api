8322c4edb7b9f0cb85230cab03459e9056aebba9
Your Name <you@example.com>
2019-05-18 21:57:00 +0430

    test end commit

--- test/test_project/test_app/models.py
+++ test/test_project/test_app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self , email , name , password=None):
+        if not email:
+            raise ValueError('email not enterd!')
+        email = self.normalize_email(email)
+        user = self.model(email = email , name = name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self , email , mail , password):
+        user = self.create_user(email , name , password)
+        user.is_superuser=True
+        user.is_staff=True
+        user.save(using = self._db)
+        return user
 
 # Create your models here.
+class User(AbstractBaseUser,PermissionsMixin):
+
+    email = models.EmailField(max_length = 255 , unique = True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['name']
+
+    def get_name(self):
+        return self.name
+    def __str__(self):
+        return self.email
