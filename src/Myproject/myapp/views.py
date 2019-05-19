052a6690c1ff88ea18e291cd5f3f9062833745c3
Your Name <you@example.com>
2019-05-19 16:16:06 +0430

    Hello APIView created created

--- src/Myproject/myapp/views.py
+++ src/Myproject/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+
+    def get(self , request , format = None):
+        an_apiview = [
+            'content 1',
+            'content 2',
+            'content 3'
+        ]
+        return Response({'message':'Hello' , 'an_api view' : an_apiview})
